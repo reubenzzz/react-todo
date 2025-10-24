90f5bd015fd3bb1aa3be3062e0fdab306cd3753e
Reuben <90466947+reubenzzz@users.noreply.github.com>
2025-10-24 22:39:23 +0530

    Create main App component with TodoApp integration

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoApp from "./components/TodoApp";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
